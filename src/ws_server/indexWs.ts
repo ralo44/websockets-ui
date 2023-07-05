cfa48c50c04e0aba97d1339c51214f754d09d1ed
ralo44 <o.sea44@gmail.com>
2023-07-05 12:21:40 -0600

    feature:login

--- src/ws_server/indexWs.ts
+++ src/ws_server/indexWs.ts
@@ -3,21 +3,38 @@
 // import { httpServer } from '../http_server/index.js';
 // import { WebSocketServer } from 'ws';
 // import { env } from 'process';
-// import { Player } from "./player.js";
+import { Player } from "./player.js";
 // import { Game } from "./game.js";
 import { IRequest } from "../type.js";
 
-export const connectWs = (ws:any) => {
-    // let newPlayer: Player;
+const connections = new Set();
+
+export const connectWs = (ws: any) => {
+    let newPlayer: Player;
     // let newGame: Game;
 
     ws.onmessage = (message: { data: string }) => {
         const { data } = message;
         const request = JSON.parse(data) as IRequest;
-        const requestType = request.type;
-        console.log(0, requestType);
+        const dataType = request.type;
+        console.log(0, dataType);
 
+
+        if (dataType === "reg") {
+            const dataReg = JSON.parse(data)
+            newPlayer = new Player(dataReg.name, dataReg.password)
+            connections.add(dataReg);
+            const response = JSON.stringify({
+                type: "reg",
+                data: JSON.stringify(newPlayer.getPlayer()),
+                id: 0,
+              });
+            ws.send(response);
+        } else {
+
+        }
     }
+
 }
 
 // const port = Number(env.HTTP_PORT || 8181);
--- src/ws_server/player.ts
+++ src/ws_server/player.ts
@@ -1,3 +1,4 @@
+import crypto from "crypto";
 export class Player {
     name: string;
     password: string;
